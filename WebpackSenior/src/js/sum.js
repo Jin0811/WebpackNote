fecc7bc6d67eec946e38e8d7046cb280cd30a71e
Jin0811 <15797954826@163.com>
2023-07-09 21:16:55 +0800

    去除代码错误

--- WebpackSenior/src/js/sum.js
+++ WebpackSenior/src/js/sum.js
@@ -1,4 +1,7 @@
 export default function sum(...args) {
   // 故意写一个错误，调用两次reduce
-  return args.reduce((pre, cur) => pre + cur, 0)();
+  // return args.reduce((pre, cur) => pre + cur, 0)();
+
+  // 正常写法
+  return args.reduce((pre, cur) => pre + cur, 0);
 }
