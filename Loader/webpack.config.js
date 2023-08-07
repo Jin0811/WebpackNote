7612e57c63aee02738b50582225563d0ffb58449
Jin0811 <15797954826@163.com>
2023-08-07 21:18:55 +0800

    feat: 规范loader命名

--- Loader/webpack.config.js
+++ Loader/webpack.config.js
@@ -17,19 +17,22 @@ module.exports = {
     rules: [
       // {
       //   test: /\.js$/,
-      //   loader: "./loaders/00_test-loader.js"
+      //   loader: "./loaders/00_test_loader.js"
       // },
       {
         test: /\.js$/,
         use: [
           // loader的四种类型
-          // "./loaders/01_sync-loader.js",
-          // "./loaders/02_async-loader.js",
-          // "./loaders/03_raw-loader.js",
-          // "./loaders/04_pitching-loader.js",
+          // "./loaders/01_sync_loader.js",
+          // "./loaders/02_async_loader.js",
+          // "./loaders/03_raw_loader.js",
+          // "./loaders/04_pitching_loader.js",
 
           // 清除console.log();
-          "./loaders/05_clean-log-loader.js",
+          "./loaders/05_clean_log_loader.js",
+
+          // 
+          "./loaders/06_banner_loader.js",
         ],
       },
     ],
